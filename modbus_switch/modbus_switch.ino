13894555935c18d3c29fb3f572b4ec68beaa4606
Sergey <makserge@mail.ru>
2020-07-06 21:20:09 +0300

    Fixed one button switch sketch

--- modbus_switch/modbus_switch.ino
+++ modbus_switch/modbus_switch.ino
@@ -1,22 +1,22 @@
 #include <ModbusSlave.h>
 #include <OneButton.h>
 
-#define SLAVE_ID 20
+#define SLAVE_ID 40
 #define RS485_BAUDRATE 9600
 
-#define RS485_TX_ENABLE_PIN PA_0
+#define RS485_TX_ENABLE_PIN PA0
 #define RS485_RX_PIN PA10
 #define RS485_TX_PIN PA9
 
-#define INPUT1_PIN PA_5
-#define OUTPUT1_PIN PB_0
+#define INPUT1_PIN PA5
+#define OUTPUT1_PIN PB1
 
 const uint8_t OUT1_STATE = 0;
 
 uint8_t outputState[1] = { LOW }; //{ OUT1_STATE }
 
 Modbus slave(SLAVE_ID, RS485_TX_ENABLE_PIN);
-OneButton button1(INPUT1_PIN, true, false);
+OneButton button1(INPUT1_PIN, true, true);
 
 uint8_t readDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
   slave.writeCoilToBuffer(OUT1_STATE, outputState[OUT1_STATE]);
@@ -34,13 +34,12 @@ uint8_t writeDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
   }
 
   if (outputState[OUT1_STATE] != lastOutputState1) {
-    setOutput(OUTPUT1_PIN, OUT1_STATE);
+    setOutput(OUTPUT1_PIN, outputState[OUT1_STATE]);
   } 
   return STATUS_OK;
 }
 
 void initButtons() {
-  pinMode(INPUT1_PIN, INPUT);
   pinMode(OUTPUT1_PIN, OUTPUT);
 
   button1.attachClick(clickButton1);

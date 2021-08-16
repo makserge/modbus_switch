72ce167e198f39b1f57a2162fe86e5f6f791d92f
makserge@mail.ru <makserge@mail.ru>
2021-08-16 11:45:39 +0300

    Fixed Modbus coil read status, added button long press

--- modbus_switch/modbus_switch.ino
+++ modbus_switch/modbus_switch.ino
@@ -1,5 +1,5 @@
-#include <ModbusSlave.h>
-#include <OneButton.h>
+#include "ModbusSlave.h"
+#include "OneButton.h"
 
 #define SLAVE_ID 40
 #define RS485_BAUDRATE 9600
@@ -12,14 +12,20 @@
 #define OUTPUT1_PIN PB1
 
 const uint8_t OUT1_STATE = 0;
+const uint8_t OUT1_LONG_PRESS_STATE = 1;
 
-uint8_t outputState[1] = { LOW }; //{ OUT1_STATE }
+uint8_t outputState[2] = { LOW, LOW }; //{ OUT1_STATE, OUT1_LONG_PRESS_STATE }
 
 Modbus slave(SLAVE_ID, RS485_TX_ENABLE_PIN);
 OneButton button1(INPUT1_PIN, true, true);
 
 uint8_t readDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
-  slave.writeCoilToBuffer(OUT1_STATE, outputState[OUT1_STATE]);
+  if (address > 2 || (address + length) > 2) {
+    return STATUS_ILLEGAL_DATA_ADDRESS;
+  }
+  for (uint16_t i = 0; i < length; i++) {
+    slave.writeCoilToBuffer(i, outputState[address + i]);
+  }
   return STATUS_OK;
 }
 
@@ -29,6 +35,9 @@ uint8_t readDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
 uint8_t writeDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
   uint8_t lastOutputState1 = outputState[OUT1_STATE];
 
+  if (address > 2 || (address + length) > 2) {
+    return STATUS_ILLEGAL_DATA_ADDRESS;
+  }
   for (uint16_t i = 0; i < length; i++) {
     outputState[i + address] = slave.readCoilFromBuffer(i);
   }
@@ -39,19 +48,29 @@ uint8_t writeDigitalOut(uint8_t fc, uint16_t address, uint16_t length) {
   return STATUS_OK;
 }
 
+void clickButton1() {
+  outputState[OUT1_STATE] = !outputState[OUT1_STATE];
+  setOutput(OUTPUT1_PIN, outputState[OUT1_STATE]);
+}
+
+void longPressStart1() {
+  outputState[OUT1_LONG_PRESS_STATE] = HIGH;
+}
+
+void longPressStop1() {
+  outputState[OUT1_LONG_PRESS_STATE] = LOW;
+}
+
 void initButtons() {
   pinMode(OUTPUT1_PIN, OUTPUT);
 
   button1.attachClick(clickButton1);
+  button1.attachLongPressStart(longPressStart1);
+  button1.attachLongPressStop(longPressStop1);
 }
 
 void setOutput(uint8_t pin, uint8_t value) {
   digitalWrite(pin, value);
-}
-
-void clickButton1() {
-  outputState[OUT1_STATE] = !outputState[OUT1_STATE];
-  setOutput(OUTPUT1_PIN, outputState[OUT1_STATE]);
 }
 
 void setup() {
